4570d409741eeee9a193a041a39f4f489eb9cec6
parnevich <parnevich_karina@mail.ru>
2021-07-23 10:01:18 +0300

    Shuffle words im sentence

--- run_7.rb
+++ run_7.rb
@@ -1,3 +1,5 @@
 MAX_FLAT = 45
 sum = 5
 puts MAX_FLAT * sum
+
+end
---
+++ run_8.rb
@@ -0,0 +1,6 @@
+def reverse_word_order(sentence)
+    reversed = []
+    sentence.split.shuffle.join(' ')
+  end
+  
+  puts reverse_word_order("My name is Karina")
